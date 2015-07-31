0dd774a91e3434c2be64c22ca4d41eb778fd6beb
Dan <dchen12812@gmail.com>
2015-07-30 23:27:23 -0700

    modified kit kat list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+k1 = KitKat.create(image_url: 'assets/k1.jpg', name: 'Tochi Otome', cost: 400, description: 'Tochigi limited edition strawberry flavored')
+k2 = KitKat.create(image_url: 'assets/k2.jpg', name: 'Amaou Strawberry', cost: 400, description: 'Kyushu limited edition strawberry flavored')
+k3 = KitKat.create(image_url: 'assets/k3.jpg', name: 'Sublime plain', cost: 300, description: 'Sublime plain flavor from the Confectionary')
+k4 = KitKat.create(image_url: 'assets/k4.jpg', name: 'Sublime Raspberry', cost: 300, description: 'Sublime raspberry flavor from the Conectionary')
+k5 = KitKat.create(image_url: 'assets/k5.jpg', name: 'Pudding', cost: 300, description: 'Pudding bakeable flavor')
+k6 = KitKat.create(image_url: 'assets/k6.jpg', name: 'Azuki Sandwich', cost: 800, description: "Nagoya's signature redbean sandiwch flavor")
+k7 = KitKat.create(image_url: 'assets/k7.jpg', name: 'Orange Cocktail', cost: 1200, description: 'Orange cocktail from the Confectionary')
+k8 = KitKat.create(image_url: 'assets/k8.jpg', name: 'Cream Cheese', cost: 1200, description: 'Cream Cheese from the Confectionary')

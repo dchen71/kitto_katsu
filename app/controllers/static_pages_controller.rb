2494a49e8c4e2de3fc8eb3a765024b842914435c
Dan <dchen12812@gmail.com>
2015-08-17 19:13:58 -0700

    init files and fixed background image to cover

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,8 @@ class StaticPagesController < ApplicationController
   def about
 
   end
+
+  def locations
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'about' => 'static_pages#about'
   get 'kitkats' => 'kit_kats#index'
+  get 'locations' => 'static_pages#locations'
 end

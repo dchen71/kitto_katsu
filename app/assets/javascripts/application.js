91c8efa0c0b75ae9feb201b87ffb4b5ed8ae9e66
Dan <dchen12812@gmail.com>
2015-08-17 19:44:08 -0700

    added store info, maps via google

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,21 @@
 $('.navbar-bar li').hover(function(){
 	$(this).toggleClass("active");
 });
+
+
+
+
+
+/*
+Google Maps
+ */
+
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);

a01016becaa649963528ecee3c73c138ba6670b4
Dan <dchen12812@gmail.com>
2015-08-19 16:52:18 -0700

    changed css of the locations

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -18,6 +18,8 @@ function initialize() {
   const osaka = new google.maps.LatLng(34.705517,135.498312);
   const sapporo = new google.maps.LatLng(43.067693,141.349444);
   
+
+  /* Setups the markers */
   var marker_tokyo1 = new google.maps.Marker({
     position:tokyo1,
     });
@@ -36,12 +38,69 @@ function initialize() {
   var marker_sapporo = new google.maps.Marker({
     position:sapporo,
     });
+  
   marker_tokyo1.setMap(map);
   marker_tokyo2.setMap(map);
   marker_sapporo.setMap(map);
   marker_osaka.setMap(map);
   marker_kyoto.setMap(map);
   marker_nagoya.setMap(map);
+
+  const tok1_cont =  "<div class='store'>" +
+						"<p class='store-title'>Address</p>" + 
+						"<p class='store-content'>大丸東京店 地下1階 ほっぺタウン</p>" +
+						"<p class='store-content'>東京都千代田区丸の内1-9-1</p>" +
+						"<p class='store-title'>Hours</p> " +
+						"<p class='store-content'>M-F: 10AM - 9PM</p>" +
+						"<p class='store-content'>Sa-Sun: 10AM-8PM</p>" +
+						"<p class='store-title'>Phone Number</p>" +
+						"<p class='store-content'>03-6895-8735</p>" +
+					 "</div>";
+
+  const tok2_cont =  "<p class='store-content'>大丸東京店 地下1階 ほっぺタウン</p>" +
+					  "<p class='store-content'>東京都千代田区丸の内1-9-1</p>" +
+					  "<p>Hours</p>" +
+					  "<p class='store-content'>M-F: 10AM - 9PM</p>" +
+					  "<p class='store-content'>Sa-Sun: 10AM-8PM</p>" +
+					  "<p>Phone Number</p>" +
+					  "<p class='store-content'>03-6895-8735</p>";
+
+  const nag1_cont =  "<p class='store-content'>大丸東京店 地下1階 ほっぺタウン</p>" +
+					  "<p class='store-content'>東京都千代田区丸の内1-9-1</p>" +
+					  "<p>Hours</p>" +
+					  "<p class='store-content'>M-F: 10AM - 9PM</p>" +
+					  "<p class='store-content'>Sa-Sun: 10AM-8PM</p>" +
+					  "<p>Phone Number</p>" +
+					  "<p class='store-content'>03-6895-8735</p>";
+
+  const kyo1_cont =  "<p class='store-content'>大丸東京店 地下1階 ほっぺタウン</p>" +
+					  "<p class='store-content'>東京都千代田区丸の内1-9-1</p>" +
+					  "<p>Hours</p>" +
+					  "<p class='store-content'>M-F: 10AM - 9PM</p>" +
+					  "<p class='store-content'>Sa-Sun: 10AM-8PM</p>" +
+					  "<p>Phone Number</p>" +
+					  "<p class='store-content'>03-6895-8735</p>";
+
+  const sap1_cont =  "<p class='store-content'>大丸東京店 地下1階 ほっぺタウン</p>" +
+					  "<p class='store-content'>東京都千代田区丸の内1-9-1</p>" +
+					  "<p>Hours</p>" +
+					  "<p class='store-content'>M-F: 10AM - 9PM</p>" +
+					  "<p class='store-content'>Sa-Sun: 10AM-8PM</p>" +
+					  "<p>Phone Number</p>" +
+					  "<p class='store-content'>03-6895-8735</p>";
+
+  const osa1_cont =  "<p class='store-content'>大丸東京店 地下1階 ほっぺタウン</p>" +
+					  "<p class='store-content'>東京都千代田区丸の内1-9-1</p>" +
+					  "<p>Hours</p>" +
+					  "<p class='store-content'>M-F: 10AM - 9PM</p>" +
+					  "<p class='store-content'>Sa-Sun: 10AM-8PM</p>" +
+					  "<p>Phone Number</p>" +
+					  "<p class='store-content'>03-6895-8735</p>";
+
+  var infowindow = new google.maps.InfoWindow({
+  content:tok1_cont});
+
+infowindow.open(map,marker_nagoya);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 

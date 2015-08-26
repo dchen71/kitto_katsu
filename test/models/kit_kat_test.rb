7790779de8e89f95f3af3d8e265f1bb80a2f1f73
Dan <dchen12812@gmail.com>
2015-08-26 14:32:06 -0700

    basic model test

--- test/models/kit_kat_test.rb
+++ test/models/kit_kat_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class KitKatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@kitkat = KitKat.new(image_url: 'k1.jpg', name: 'Tochi Otome', 		cost: 400, 		location: 'Tochigi', 		description: 'Tochigi limited edition strawberry flavored. Tochigi prefecture is an area well known for its strawberries.')
+  end
+
+  test '@kitkat should be valid' do
+  	assert @kitkat.valid?
+  end
 end
